ea9812e54cb4f6f0164b2b73248cd6a6391ec9b3
samturton2 <STurton@spartaglobal.com>
2020-11-05 11:22:08 +0000

    added reptile class as child class of animal

--- animal_class.py
+++ animal_class.py
@@ -1,1 +1,27 @@
 # Creating an Animal class as PARENT/BASE/SUPER class
+
+class Animal:
+
+    def __init__(self): # initialising animal class
+        self.alive = True # creating an attribute/ variable
+        self.spine = True
+        self.eyes = True
+        self.lungs = True
+
+    # create behaviours
+    def breathe(self):
+        return "keep breathing to stay alive"
+
+    def move(self):
+        return "left to right and up and down"
+
+    def eat(self):
+        return "nom nom nom"
+
+    def procreate(self):
+        return "find partner"
+
+# # instantiate our class/ create an object
+# cat = Animal() # creating an object of Animal class
+# # cat as child inherits everything everything from Animal/ parent class
+# print(cat.eat())
---
+++ reptile_class.py
@@ -0,0 +1,32 @@
+# creating a reptile class as a child class of our Animal class
+from animal_class import Animal
+# syntax to import files and classes
+
+class Reptile(Animal):
+    def __init__(self):
+        # super key word is used to inherit everything from parent class
+        super().__init__()
+        self.cold_blooded = True
+        self.tetrapod = True
+        self.heart_chambers = [3, 4]
+
+    # creating function for our reptile class
+    def seek_heat(self):
+        return "i need heat"
+
+    def hunt(self):
+        return "get me some scran"
+
+    def use_venom(self):
+        return "POISON"
+
+# # Let's create an object of our reptile class to utilise the amazing functionalities of OOP
+# reptile_object = Reptile()
+#
+# print(reptile_object.eat())
+# print(reptile_object.breathe())
+# print(reptile_object.procreate())
+# print("the above functionalities are inherited from our Animal/ Parent class")
+#
+# print(reptile_object.use_venom())
+# print(reptile_object.hunt())
